8a0b96d9cafc50dc95bbdd9c66f68e266f796125
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-19 14:35:17 -0800

    add Razor loop in locations.cshtml and starter Session class to maintain user session

---
+++ Objects/Session.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Kickstart;
+
+namespace Kickstart
+{
+  public class Session
+  {
+    public static List<Session> currentSession = new List<Session>l
+  }
+}
