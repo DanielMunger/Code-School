c185c90dcafd00f2a195859f3b1db0b7a8aeb033
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-19 09:50:44 -0800

    add views and routes for each navbar item

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,8 +8,29 @@ namespace Modules
   {
     public HomeModule()
     {
+      // Routes for Landing Page
       Get["/"] = _ => {
         return View["index.cshtml"];
+      };
+      Get["/courses"] = _ =>
+      {
+        return View["courses.cshtml"];
+      };
+      Get["/instructors"] = _ =>
+      {
+        return View["instructors.cshtml"];
+      };
+      Get["/locations"] = _ =>
+      {
+        return View["locations.cshtml"];
+      };
+      Get["/account/create"] = _ =>
+      {
+        return View["new_account.cshtml"];
+      };
+      Get["/account/login"] = _ =>
+      {
+        return View["login.cshtml"];
       };
     }
   }
