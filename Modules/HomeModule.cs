e93f0216d5ecaca1bf2131eb8c9c6534c4279b03
ryanpeterson08 <ryanpeterson08@gmail.com>
2016-12-19 09:09:46 -0800

    add Shared views, index.cshtml and .json files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using System.Collections.Generic;
+using Namespace.Startup;
+
+namespace Modules
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNet.Builder;
+using Nancy.Owin;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace Namespace.Startup
+{
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseOwin(x => x.UseNancy());
+    }
+  }
+  public static class DBConfiguration
+  {
+      public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=kickstartdb;Integrated Security=SSPI;";
+  }
+  public class CustomRootPathProvider : IRootPathProvider
+  {
+    public string GetRootPath()
+    {
+      return Directory.GetCurrentDirectory();
+    }
+  }
+  public class RazorConfig : IRazorConfiguration
+  {
+    public IEnumerable<string> GetAssemblyNames()
+    {
+      return null;
+    }
+
+    public IEnumerable<string> GetDefaultNamespaces()
+    {
+      return null;
+    }
+
+    public bool AutoIncludeModelNamespace
+    {
+      get { return false; }
+    }
+  }
+}
